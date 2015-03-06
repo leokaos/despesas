af128f5e2d33e8c6cb9bd12b09d4c398a4633aee
leokaos <leokaos@gmail.com>
2015-03-06 11:20:12 -0300

    Periodo Criado

--- despesas-web/src/main/webapp/assets/js/app.js
+++ despesas-web/src/main/webapp/assets/js/app.js
@@ -1,3 +1,30 @@
+//PERIODO
+function Periodo(mes, ano) {
+    this.mes = --mes;
+    this.ano = ano;
+};
+
+Periodo.prototype.getDataInicial = function () {
+    var dataInicial = new Date();
+
+    dataInicial.setFullYear(this.ano);
+    dataInicial.setMonth(this.mes);
+    dataInicial.setDate(1);
+
+    return dataInicial;
+};
+
+Periodo.prototype.getDataFinal = function () {
+    var dataInicial = new Date();
+
+    dataInicial.setFullYear(this.ano);
+    dataInicial.setMonth(this.mes + 1);
+    dataInicial.setDate(0);
+
+    return dataInicial;
+};
+
+
 var app = angular.module('despesas', ['ngRoute', 'ngAnimate', 'ngResource', 'colorpicker.module', 'ui.utils.masks']);
 
 app.config(function ($routeProvider, $locationProvider) {
