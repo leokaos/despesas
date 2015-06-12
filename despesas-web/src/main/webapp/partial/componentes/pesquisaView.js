5e8c9a5ddb979f13c2d3afee4aef40b51797c5c2
leo <leo@leo.com.br>
2015-06-12 17:37:16 -0300

    ajustes 2

--- despesas-web/src/main/webapp/partial/componentes/pesquisaView.js
+++ despesas-web/src/main/webapp/partial/componentes/pesquisaView.js
@@ -1,95 +1,100 @@
-app.directive('pesquisaView', function (usSpinnerService) {
+app.directive('pesquisaView',function(usSpinnerService) {
 
-    return {
-        restrict: 'E',
-        scope: false,
-        replace: true,
-        transclude: true,
-        templateUrl: 'partial/componentes/pesquisaView.html',
+	return {
+	    restrict: 'E',
+	    scope: false,
+	    replace: true,
+	    transclude: true,
+	    templateUrl: 'partial/componentes/pesquisaView.html',
 
-        link: function ($scope, iElement, iAttrs) {
+	    link: function($scope,iElement,iAttrs) {
 
-            $scope.numberPages = [5, 10, 25, 50];
-            $scope.pageSize = $scope.numberPages[0];
-            $scope.currentPage = 0;
-            $scope.pages = 0;
+		    $scope.numberPages = [
+		            5,
+		            10,
+		            25,
+		            50
+		    ];
+		    $scope.pageSize = $scope.numberPages[0];
+		    $scope.currentPage = 0;
+		    $scope.pages = 0;
 
-            $scope.changePage = function (page) {
-                $scope.currentPage = page;
-                $scope.loadData($scope.originalData);
-            };
+		    $scope.changePage = function(page) {
+			    $scope.currentPage = page;
+			    $scope.loadData($scope.originalData);
+		    };
 
-            $scope.changePageSize = function () {
-                $scope.currentPage = 0;
-                $scope.loadData($scope.originalData);
-            };
+		    $scope.changePageSize = function() {
+			    $scope.currentPage = 0;
+			    $scope.loadData($scope.originalData);
+		    };
 
-            $scope.nextPage = function () {
-                $scope.currentPage++;
-                $scope.loadData($scope.originalData);
-            };
+		    $scope.nextPage = function() {
+			    $scope.currentPage++;
+			    $scope.loadData($scope.originalData);
+		    };
 
-            $scope.previousPage = function () {
-                $scope.currentPage--;
-                $scope.loadData($scope.originalData);
-            };
+		    $scope.previousPage = function() {
+			    $scope.currentPage--;
+			    $scope.loadData($scope.originalData);
+		    };
 
-            $scope.firstPage = function () {
-                $scope.currentPage = 0;
-                $scope.loadData($scope.originalData);
-            };
+		    $scope.firstPage = function() {
+			    $scope.currentPage = 0;
+			    $scope.loadData($scope.originalData);
+		    };
 
-            $scope.lastPage = function () {
-                $scope.currentPage = $scope.pages - 1;
-                $scope.loadData($scope.originalData);
-            };
+		    $scope.lastPage = function() {
+			    $scope.currentPage = $scope.pages - 1;
+			    $scope.loadData($scope.originalData);
+		    };
 
-            $scope.isLastPage = function () {
-                if ($scope.pages == 0) {
-                    return true;
-                }
+		    $scope.isLastPage = function() {
+			    if ($scope.pages == 0) {
+				    return true;
+			    }
 
-                return (($scope.currentPage + 1) == $scope.pages);
-            };
+			    return (($scope.currentPage + 1) == $scope.pages);
+		    };
 
-            $scope.isFirstPage = function () {
-                if ($scope.pages == 0) {
-                    return true;
-                }
+		    $scope.isFirstPage = function() {
+			    if ($scope.pages == 0) {
+				    return true;
+			    }
 
-                return $scope.currentPage == 0;
-            };
+			    return $scope.currentPage == 0;
+		    };
 
-            $scope.deletar = function (data) {
+		    $scope.deletar = function(data) {
 
-                var id = $scope.getItemSelecionado().id;
+			    var id = $scope.getItemSelecionado().id;
 
-                for (var x = 0; x < $scope.originalData.length; x++) {
-                    var tp = $scope.originalData[x];
-                    if (tp.id == id) {
-                        $scope.originalData.splice(x, 1);
-                        break;
-                    }
-                }
+			    for (var x = 0 ; x < $scope.originalData.length ; x++) {
+				    var tp = $scope.originalData[x];
+				    if (tp.id == id) {
+					    $scope.originalData.splice(x,1);
+					    break;
+				    }
+			    }
 
-                $('#modalExcluir').modal('hide');
-                $('#modalOk').modal('show');
-                $scope.loadData($scope.originalData);
-            };
+			    $('#modalExcluir').modal('hide');
+			    $('#modalOk').modal('show');
+			    $scope.loadData($scope.originalData);
+		    };
 
-            $scope.loadData = function (lista) {
+		    $scope.loadData = function(lista) {
 
-                $scope.originalData = lista;
+			    $scope.originalData = lista;
 
-                $scope.pageSize = parseInt($scope.pageSize);
-                $scope.first = $scope.pageSize * $scope.currentPage;
-                $scope.sliceData = $scope.originalData.slice($scope.first, $scope.first + $scope.pageSize);
-                $scope.pages = Math.ceil($scope.originalData.length / $scope.pageSize);
+			    $scope.pageSize = parseInt($scope.pageSize);
+			    $scope.first = $scope.pageSize * $scope.currentPage;
+			    $scope.sliceData = $scope.originalData.slice($scope.first,$scope.first + $scope.pageSize);
+			    $scope.pages = Math.ceil($scope.originalData.length / $scope.pageSize);
 
-                usSpinnerService.stop($scope.getNomeSpin());
-            };
+			    usSpinnerService.stop($scope.getNomeSpin());
+		    };
 
-            $scope.listar();
-        }
-    }
+		    $scope.listar();
+	    }
+	};
 });
