bf75caa6e275e1a8477d5f6540c02b0c069459e5
leokaos <leokaos@gmail.com>
2015-07-16 11:21:29 -0300

    Ajuste orçamento.

--- despesas-web/src/main/webapp/assets/js/orcamentoService.js
+++ despesas-web/src/main/webapp/assets/js/orcamentoService.js
@@ -1,59 +1,59 @@
-app.service('orcamentoService', function($http) {
+app.service('orcamentoService',function($http) {
 
-  var pathBase = '/despesas/services/orcamento/';
+	var pathBase = '/despesas/services/orcamento/';
 
-  this.getNovoOrcamento = function() {
-    return {
-      tipoDespesa: null,
-      periodo: new Periodo(null, null),
-      valor: null,
-    };
-  };
+	this.getNovoOrcamento = function() {
+		return {
+		    tipoDespesa: null,
+		    periodo: new Periodo(null,null),
+		    valor: null,
+		};
+	};
 
-  this.listar = function(fn) {
-    $http.get(pathBase).success(function(data) {
-      fn(data);
-    });
-  };
+	this.listar = function(fn) {
+		$http.get(pathBase).success(function(data) {
+			fn(data);
+		});
+	};
 
-  this.novo = function(orcamento, fn) {
-    $http.post(pathBase, orcamento).success(function(data) {
-      fn(data);
-    });
-  };
+	this.novo = function(orcamento,fn) {
+		$http.post(pathBase,orcamento).success(function(data) {
+			fn(data);
+		});
+	};
 
-  this.salvar = function(orcamento, fn) {
-    $http.put(pathBase, orcamento).success(function(data) {
-      fn(data);
-    });
-  };
+	this.salvar = function(orcamento,fn) {
+		$http.put(pathBase,orcamento).success(function(data) {
+			fn(data);
+		});
+	};
 
-  this.buscarPorId = function(id, fn) {
-    $http.get(pathBase + id).success(function(data) {
-      fn(data);
-    });
-  };
+	this.buscarPorId = function(id,fn) {
+		$http.get(pathBase + id).success(function(data) {
+			fn(data);
+		});
+	};
 
-  this.deletar = function(id, fn) {
-    $http.delete(pathBase + id).success(function(data) {
-      fn(data);
-    });
-  };
+	this.deletar = function(id,fn) {
+		 $http.delete(pathBase + id).success(function(data) {
+			 fn(data);
+		 });
+	};
 
-  this.filtrarPorData = function(data, tipodespesa, fn) {
+	this.filtrarPorData = function(data,tipodespesa,fn) {
 
-    var request = $http({
-      method: 'get',
-      url: pathBase + 'data/',
-      params: {
-        vencimento: data.toGMTString(),
-        tipoDespesa: tipodespesa
-      }
-    });
+		var request = $http({
+		    method: 'get',
+		    url: pathBase + 'data/',
+		    params: {
+		        vencimento: data.toGMTString(),
+		        tipoDespesa: tipodespesa
+		    }
+		});
 
-    request.success(function(data) {
-      fn(data);
-    });
-  };
+		request.success(function(data) {
+			fn(data);
+		});
+	};
 
 });
