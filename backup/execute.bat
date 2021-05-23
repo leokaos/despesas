1695c746844a3ccce864d2488f8e35cdd9630834
leo <leokaos@gmail.com>
2021-05-23 12:51:03 -0300

    backup 23-05-2021

--- backup/execute.bat
+++ backup/execute.bat
@@ -1,3 +1,3 @@
 set _date=%DATE:/=-%
 
-pg_dump --dbname=postgresql://despesas:despesas_db@127.0.0.1:5432/despesas > backup-%_date%.sql
+pg_dump --dbname=postgresql://despesas:despesas_db@127.0.0.1:5432/despesas > "D:\Projetos\despesas\backup\backup-%_date%.sql"
