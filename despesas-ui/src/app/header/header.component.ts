33a508e50490f1b38c03c20fd6b4a8bb83882f64
Leonardo Guerra <leokaos@gmail.com>
2024-11-06 14:55:07 +0000

    backup 06-11-2024

--- despesas-ui/src/app/header/header.component.ts
+++ despesas-ui/src/app/header/header.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
 
+  private openSubMenus: Set<string> = new Set();
+
+  toggleSubMenu(menuItem: string): void {
+    if (this.openSubMenus.has(menuItem)) {
+      this.openSubMenus.delete(menuItem);
+    } else {
+      this.openSubMenus.add(menuItem);
+    }
+  }
+
+  isSubMenuOpen(menuItem: string): boolean {
+    return this.openSubMenus.has(menuItem);
+  }
 }
