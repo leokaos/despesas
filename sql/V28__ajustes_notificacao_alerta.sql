66ec3a2af8a605310f34317d5263f4b58b6d8f7f
Leonardo Otero <leokaos@gmail.com>
2026-08-02 10:56:13 +0100

    correcao migration

--- sql/V28__ajustes_notificacao_alerta.sql
+++ sql/V28__ajustes_notificacao_alerta.sql
@@ -5,5 +5,5 @@ ALTER TABLE despesas_db.notificacao DROP COLUMN mes;
 ALTER TABLE despesas_db.notificacao DROP COLUMN ano;
 
 
-ALTER TABLE despesas_db.alerta_limite_pagamento_divida ADD CONSTRAINT alerta_limite_pagamento_divida_id_fkey_divida FOREIGN KEY (divida_id) REFERENCES divida(id);
-ALTER TABLE despesas_db.alerta_pagamento_fatura_cartao ADD CONSTRAINT alerta_pagamento_fatura_cartao_id_fkey_cartao FOREIGN KEY (cartao_credito_id) REFERENCES cartao(id);
+ALTER TABLE despesas_db.alerta_limite_pagamento_divida ADD CONSTRAINT alerta_limite_pagamento_divida_id_fkey_divida FOREIGN KEY (divida_id) REFERENCES despesas_db.divida(id);
+ALTER TABLE despesas_db.alerta_pagamento_fatura_cartao ADD CONSTRAINT alerta_pagamento_fatura_cartao_id_fkey_cartao FOREIGN KEY (cartao_credito_id) REFERENCES despesas_db.cartao(id);
